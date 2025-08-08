215cb9b5cdce7f6fde74406bf534ba963cf1d1cb
AngelosKl <rtfon2@gmail.com>
2025-08-08 17:30:29 +0300

    Update Home page with [brief description of changes]

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
         <div style={{ textAlign: 'center', padding: '2rem' }}>
             <img src={homePhoto} alt="homePhoto" style={{ transform: 'scale(0.85)' }} />
             <h1>Welcome to My World</h1>
-            <p>Explore my life, passions, and connect with me!!!</p>
+            <p>Explore my life, passions, and connect with me!!</p>
         </div>
     );
 }
